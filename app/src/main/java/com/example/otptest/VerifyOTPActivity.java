5fc3b26da95117cf23799534d9222bf9c0929203
mustafa-kapadia1483 <mustafakapadia76@gmail.com>
2022-02-21 15:40:37 +0530

    refactoring

--- app/src/main/java/com/example/otptest/VerifyOTPActivity.java
+++ app/src/main/java/com/example/otptest/VerifyOTPActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,7 +19,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
@@ -78,7 +76,7 @@ public class VerifyOTPActivity extends AppCompatActivity {
                     progressBar.setVisibility(View.VISIBLE);
                     PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.getCredential(
                             verificationId,
-                            code.toString()
+                            code
                     );
                     FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential)
                             .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
